24b080321aefc139868af9d90adbcbdf630cf97a
Dev Rai <118540558+Dev16rai@users.noreply.github.com>
2022-11-25 09:12:33 -0800

    Add files via upload

---
+++ basicthings.c
@@ -0,0 +1,41 @@
+/*
+line 1.#inlude<stdio.h> :- it is a preprocessor
+command which tells a C compiler to
+include stdio.h file hwich is requrired
+to use the printf command 
+
+ekdam desi word mai jo bhi hum code likhenge vo kaise bhi karke lake do
+
+*/
+
+#include<stdio.h>
+
+/*line 2.(void) or int main():--the main() function is entery point of every program in C language.
+the keyword void indicates that the function does not return a value its simple performs the operations in it
+
+*/
+int main()
+
+/* 
+{
+curly broces { } in C are used to define the block of code
+}
+*/
+
+
+{
+printf("my name is Dev this is the first thinks you should know about coding ");
+return 0;
+}
+
+/*
+line 3.printf("helo world"); :-- the printf("") function is used to print data on
+ the console the data that we wish to print
+has to be  passed inside it in double quotes 
+*/
+ 
+
+
+/*
+every statement in a C program terminates with a semi-colon (;)
+*/
---
+++ basicthings1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a= 9,b=4;
+int c=a+b;
+printf("Addition is,%d\n",c);
+c=a-b;
+printf("substance is,%d\n",c);
+c=a/b;
+printf("Division is,%d\n",c);
+c=a*b;
+printf("multifiation is,%d\n",c);
+c=a%b;
+printf("reminder is,%d\n",c);
+return 0;
+}
