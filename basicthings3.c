9272996cbe77b7c2d4fb3b54bb09015bb7f41c7c
Dev Rai <118540558+Dev16rai@users.noreply.github.com>
2022-12-08 00:14:44 +0530

    Add files via upload

---
+++ basicthings3.c
@@ -0,0 +1,20 @@
+  /*
+  ( > greater than)
+  ( < less than )
+  ( >= greater than or equal)
+  ( <= less than or equal)
+  ( == equal to equal)
+  ( ! not equal )
+  */
+
+ #include<stdio.h>
+ int main()
+ 
+ {
+    int a=5,b=9;
+printf("checking if a is greather than b %d\n",a>b);
+printf("checking if a is less than b %d\n",a<b);
+
+return 0;
+
+ }
