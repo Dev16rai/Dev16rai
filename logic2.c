bda4cefa2769e7747e11934fdaf0383ea96f604f
Dev Rai <118540558+Dev16rai@users.noreply.github.com>
2022-12-17 16:36:51 +0530

    Add files via upload

---
+++ logic2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+
+{
+
+  int num;
+  printf("please entre your numner :");
+  scanf("%d",&num);
+  if(num > 5){
+  
+printf("the number is gearter then 5");
+
+
+  }else{
+    printf("the number is not gearter then 5");
+  }
+
+return 0;
+}
