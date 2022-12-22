2db1ae383f25c04d46b15a5428ece2b81c113303
Dev Rai <118540558+Dev16rai@users.noreply.github.com>
2022-12-22 05:47:48 +0530

    Add files via upload

---
+++ Range-even.c
@@ -0,0 +1,32 @@
+
+/*<<<<<-----A55 Bhumika-----.>>>>>.*/
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("program to print in range");
+printf("\nenter the number a,b");
+
+scanf("%d %d",&a,&b);
+printf("print the number in even :");
+while (a<=b)
+{
+    if(a%2=0)
+    printf("%d\n",a);
+    a++;
+}
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
