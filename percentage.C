6059a47e0443b821c510a919f652405b1540483f
Dev Rai <118540558+Dev16rai@users.noreply.github.com>
2022-12-23 09:40:15 +0530

    Add files via upload

---
+++ percentage.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b,c,d,e,f;
+    printf("Enter the values of m1 m2 m3 m4 m5\n");
+    scanf("%d %d %d %d %d ",&a,&b,&c,&d,&e,&f);
+    f=(a+b+c+d)*100/500;
+    if(f<40){
+      printf("you are failed");}
+      if(f>=40&& f<50){
+        printf("ypu passed ");
+      }if(f>=50&& f<60){
+        printf("you got a second class");  
+      }if(f>=60&& f<70){
+        printf("you got a first class ");
+      }if(f>70){
+        printf(" you are the best ");
+      }
+      return 0;
+}
